575b7d55c505e17a194e1cabd7f0bd44db02947e
Mihail Hitrov <mike.hitrov@abv.bg>
2020-03-26 23:47:26 +0200

    Added projects

---
+++ Be-on-time.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Be_on_time.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
