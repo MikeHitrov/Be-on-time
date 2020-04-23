80cbc02a70af171952720257d4bdc9f599dcd253
mike.hitrov <mike.hitrov@gmail.com>
2020-04-24 00:57:42 +0300

    Added tests

---
+++ Tests/BeOnTime.Services.Data.Tests/FeedbacksServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeOnTime.Services.Data.Tests
+{
+    public class FeedbacksServiceTests
+    {
+    }
+}
---
+++ Tests/BeOnTime.Services.Data.Tests/usersServiceTests.cs
@@ -0,0 +1,70 @@
+﻿namespace BeOnTime.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using AspNetCoreTemplate.Data;
+    using AspNetCoreTemplate.Data.Common.Repositories;
+    using AspNetCoreTemplate.Data.Models;
+    using AspNetCoreTemplate.Data.Repositories;
+
+    using Microsoft.EntityFrameworkCore;
+
+    using Moq;
+
+    using Xunit;
+
+    public class UsersServiceTests
+    {
+        public List<ApplicationUser> GetTestUsersProfile()
+        {
+            return new List<ApplicationUser>
+            {
+                new ApplicationUser
+                {
+                    UserName = "Mikel",
+                    Email = "mike.hitrov@abv.bg",
+                },
+                new ApplicationUser
+                {
+                   UserName = "Stamat",
+                    Email = "stamat.hitrov@abv.bg",
+                },
+                new ApplicationUser
+                {
+                    UserName = "Georgi",
+                    Email = "georgi.hitrov@abv.bg",
+                },
+            };
+        }
+
+        [Fact]
+        public void GetAllUsersCorrectNumber()
+        {
+            var repository = new Mock<IDeletableEntityRepository<ApplicationUser>>();
+            repository.Setup(f => f.All())
+                .Returns(this.GetTestUsersProfile()
+                .AsQueryable());
+
+            var service = new UsersService(repository.Object);
+            var list = service.GetAllUsers().ToList();
+
+            Assert.Equal(3, list.Count());
+        }
+
+        [Fact]
+        public void GetUserByUsernameCorrectly()
+        {
+            var repository = new Mock<IDeletableEntityRepository<ApplicationUser>>();
+            repository.Setup(f => f.All())
+                .Returns(this.GetTestUsersProfile()
+                .AsQueryable());
+
+            var service = new UsersService(repository.Object);
+            var user = service.GetUserByUsername("Georgi");
+
+            Assert.Equal(this.GetTestUsersProfile().Where(u => u.UserName == "Georgi").First(), user);
+        }
+    }
+}
