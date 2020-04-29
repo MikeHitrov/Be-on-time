0bbfde801f218d0e7d144a7a6b30eca434e7e618
mike.hitrov <mike.hitrov@gmail.com>
2020-04-29 10:40:14 +0300

    Added tests

--- Tests/BeOnTime.Services.Data.Tests/FeedbacksServiceTests.cs
+++ Tests/BeOnTime.Services.Data.Tests/FeedbacksServiceTests.cs
@@ -19,7 +19,7 @@
     public class FeedbacksServiceTests
     {
         [Fact]
-        public void AddMeetingCorrectly()
+        public async void AddFeedbackCorrectly()
         {
             var dbContext = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString());
@@ -30,12 +30,203 @@
 
             var service = new FeedbacksService(feedbackRepository);
 
-            service.AddAsync(Guid.NewGuid().ToString(), new ApplicationUser(), 6, "Awesome", Guid.NewGuid().ToString(), new Meeting());
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+            };
+
+            var meeting = new Meeting
+            {
+                Id = Guid.NewGuid().ToString(),
+            };
+
+            await service.AddAsync(user.Id, user, 6, "Awesome", meeting.Id, meeting);
 
             var list = feedbackRepository.All().ToList();
             Assert.Single(list);
             Assert.Equal("Awesome", list[0].Description);
             Assert.Equal(6, list[0].Rating);
+        }
+
+        [Fact]
+        public async void ListAllFeedbackCorrectly()
+        {
+            var dbContext = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var feedbackRepository = new EfDeletableEntityRepository<Feedback>(new ApplicationDbContext(dbContext.Options));
+
+            feedbackRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            var service = new FeedbacksService(feedbackRepository);
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+            };
+
+            var meeting = new Meeting
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Test 1",
+            };
+
+            var secondMeeting = new Meeting
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Test 2",
+            };
+
+            await service.AddAsync(user.Id, user, 6, "Awesome", meeting.Id, meeting);
+            await service.AddAsync(user.Id, user, 1, "Poor", secondMeeting.Id, secondMeeting);
+
+            var list = service.GetAllFeedbacks().ToList();
+            Assert.Equal(2, list.Count);
+        }
+
+        [Fact]
+        public async void ListAllUserFeedbackCorrectly()
+        {
+            var dbContext = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var feedbackRepository = new EfDeletableEntityRepository<Feedback>(new ApplicationDbContext(dbContext.Options));
+
+            feedbackRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            var service = new FeedbacksService(feedbackRepository);
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+            };
+
+            var meeting = new Meeting
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Test 1",
+            };
+
+            var secondMeeting = new Meeting
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Test 2",
+            };
+
+            await service.AddAsync(user.Id, user, 6, "Awesome", meeting.Id, meeting);
+            await service.AddAsync("Not user id", new ApplicationUser { }, 1, "Poor", secondMeeting.Id, secondMeeting);
+
+            var list = service.GetUserFeedbacks(user.Id).ToList();
+            Assert.Single(list);
+        }
+
+        [Fact]
+        public async void GetFeedbackByIdCorrectly()
+        {
+            var dbContext = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var feedbackRepository = new EfDeletableEntityRepository<Feedback>(new ApplicationDbContext(dbContext.Options));
+
+            feedbackRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            var service = new FeedbacksService(feedbackRepository);
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+            };
+
+            var meeting = new Meeting
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Test 1",
+            };
+
+            var secondMeeting = new Meeting
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Test 2",
+            };
+
+            await service.AddAsync(user.Id, user, 6, "Awesome", meeting.Id, meeting);
+            await service.AddAsync("Not user id", new ApplicationUser { }, 1, "Poor", secondMeeting.Id, secondMeeting);
+
+            var id = feedbackRepository.All().Where(f => f.MeetingId == secondMeeting.Id).First().Id;
+            var feedback = service.GetFeedbackById(id);
+
+            Assert.Equal(1, feedback.Rating);
+            Assert.Equal("Poor", feedback.Description);
+        }
+
+        [Fact]
+        public async void UpdateFeedbackCorrectly()
+        {
+            var dbContext = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var feedbackRepository = new EfDeletableEntityRepository<Feedback>(new ApplicationDbContext(dbContext.Options));
+
+            feedbackRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            var service = new FeedbacksService(feedbackRepository);
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+            };
+
+            var meeting = new Meeting
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Test 1",
+            };
+
+            await service.AddAsync(user.Id, user, 6, "Awesome", meeting.Id, meeting);
+
+            var id = feedbackRepository.All().Where(f => f.MeetingId == meeting.Id).First().Id;
+
+            await service.Update(5, "Very good", id);
+
+            var feedback = service.GetFeedbackById(id);
+
+            Assert.Equal(5, feedback.Rating);
+            Assert.Equal("Very good", feedback.Description);
+        }
+
+        [Fact]
+        public async void DeleteFeedbackCorrectly()
+        {
+            var dbContext = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var feedbackRepository = new EfDeletableEntityRepository<Feedback>(new ApplicationDbContext(dbContext.Options));
+
+            feedbackRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            var service = new FeedbacksService(feedbackRepository);
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+            };
+
+            var meeting = new Meeting
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Test 1",
+            };
+
+            await service.AddAsync(user.Id, user, 6, "Awesome", meeting.Id, meeting);
+
+            var id = feedbackRepository.All().Where(f => f.MeetingId == meeting.Id).First().Id;
+            var feedback = service.GetFeedbackById(id);
+
+            await service.Delete(feedback);
+
+            var list = service.GetAllFeedbacks();
+
+            Assert.Empty(list);
         }
     }
 }
--- Tests/BeOnTime.Services.Data.Tests/usersServiceTests.cs
+++ Tests/BeOnTime.Services.Data.Tests/usersServiceTests.cs
@@ -1,14 +1,16 @@
 ﻿namespace BeOnTime.Services.Data.Tests
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
     using AspNetCoreTemplate.Data;
     using AspNetCoreTemplate.Data.Common.Repositories;
+    using AspNetCoreTemplate.Data.Migrations;
     using AspNetCoreTemplate.Data.Models;
     using AspNetCoreTemplate.Data.Repositories;
-
+    using BeOnTime.Data.Models;
     using Microsoft.EntityFrameworkCore;
 
     using Moq;
@@ -23,16 +25,19 @@
             {
                 new ApplicationUser
                 {
+                    Id = "MikelId",
                     UserName = "Mikel",
                     Email = "mike.hitrov@abv.bg",
                 },
                 new ApplicationUser
                 {
-                   UserName = "Stamat",
+                    Id = "StamatId",
+                    UserName = "Stamat",
                     Email = "stamat.hitrov@abv.bg",
                 },
                 new ApplicationUser
                 {
+                    Id = "GeorgiId",
                     UserName = "Georgi",
                     Email = "georgi.hitrov@abv.bg",
                 },
@@ -64,7 +69,48 @@
             var service = new UsersService(repository.Object);
             var user = service.GetUserByUsername("Georgi");
 
-            Assert.Equal(this.GetTestUsersProfile().Where(u => u.UserName == "Georgi").First(), user);
+            Assert.Equal("GeorgiId", this.GetTestUsersProfile().Where(u => u.UserName == "Georgi").First().Id);
+        }
+
+        [Fact]
+        public void GetUserByIdCorrectly()
+        {
+            var repository = new Mock<IDeletableEntityRepository<ApplicationUser>>();
+            repository.Setup(f => f.All())
+                .Returns(this.GetTestUsersProfile()
+                .AsQueryable());
+
+            var service = new UsersService(repository.Object);
+            var user = service.GetUserById("StamatId");
+
+            Assert.Equal("Stamat", user.UserName);
+        }
+
+        [Fact]
+        public async void UpdateTeamCorrectly()
+        {
+            var repository = new Mock<IDeletableEntityRepository<ApplicationUser>>();
+            repository.Setup(f => f.All())
+                .Returns(this.GetTestUsersProfile()
+                .AsQueryable());
+
+            var service = new UsersService(repository.Object);
+
+            var user = service.GetUserByUsername("Georgi");
+
+            Assert.Equal(null, user.TeamId);
+
+            BeOnTime.Data.Models.Team team = new BeOnTime.Data.Models.Team
+            {
+                Id = Guid.NewGuid().ToString(),
+                ManagerId = user.Id,
+                Manager = user,
+                TeamName = "Happy team",
+            };
+
+            await service.UpdateTeam(team.Id, team, user.Id);
+
+            Assert.Equal(team.Id, user.TeamId);
         }
     }
 }
